c4be68f4d572ac30d0f4d1788b053d6a436a3afc
¨scholl1984¨ <yannick.hervieu@gmail.com>
2022-05-27 23:40:17 +0200

    secondary input built

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
 import {recipes} from '../data/recipes.js';
 
-// display card recettes //
+
 
 recipes.map((receipe) => {
 
-    const {id, name, time, description, ingredients} = receipe  
+    const {id, name, time, description, ingredients, appliance, ustensils} = receipe  
+
+    // display card recettes //
+
     const receipCard = document.querySelector(".recettes_container")
     const article = document.createElement( 'article' );
     const img = document.createElement( 'img' );   
@@ -37,25 +40,59 @@ recipes.map((receipe) => {
     cardUl.setAttribute("class", "cardUl")
     cardUl.setAttribute('id', `cardUl-${id}`);    
     cardUl.setAttribute('key', `key-${id}`);
+    divDescription.setAttribute("class", "divDescription")
+    divDescription.textContent = description
 
     ingredients.map((item) => {
         
+    // display card recettes // (suite)
+
         let recipeDetails = document.createElement( 'li' );
         recipeDetails.textContent = `${item.ingredient}:${item.quantity}`;                           
         cardUl.appendChild(recipeDetails)
+
+    // display ingredients options //
+
+     const ingredientsList = document.querySelector(".search-list")
+     const li = document.createElement('li')
+     li.setAttribute("class", "hidden")
+     li.setAttribute("value", `${item.ingredient}`)
+     li.textContent = item.ingredient
+     ingredientsList.appendChild(li)
+    
     }) 
+
+    // display appareils options //
     
-    divDescription.setAttribute("class", "divDescription")
-    divDescription.textContent = description
+    const appareilsList = document.querySelector(".search-list-appareils")
+    const liappareils = document.createElement('li')
+    liappareils.setAttribute("class", "hidden")
+    liappareils.setAttribute("value", `${appliance}`)
+    liappareils.textContent = appliance
+    appareilsList.appendChild(liappareils)
+
+
+    // display ustensiles options //
+
+    const ustensilesList = document.querySelector(".search-list-ustensiles")
+    const liustensiles= document.createElement('li')
+    liustensiles.setAttribute("class", "hidden")
+    liustensiles.setAttribute("value", `${ustensils}`)
+    liustensiles.textContent = ustensils
+    ustensilesList.appendChild(liustensiles)
+
+    // function played //
+
+     filterSearchBar (receipe, description, name, ingredients)  
+     filterIngredients (ingredients)
     
-    filterSearchBar (receipe, description, name)  
 }) 
 
-function filterSearchBar (receipe, description, name) {
+function filterSearchBar (receipe, description, name, ingredients) {
    
-        let inputSearchBar = document.querySelector('.principal-input');
+    let inputSearchBar = document.querySelector('.principal-input');
         
-            inputSearchBar.addEventListener('keyup', function(ev) {
+        inputSearchBar.addEventListener('keyup', function(ev) {
 
                 var text = ev.target.value;
                 var pat = new RegExp(text, 'i');
@@ -68,18 +105,42 @@ function filterSearchBar (receipe, description, name) {
                                 if (!pat.test(item.innerText)) {
                                     item.style.display = "none"
                                 } else {         
-                                    item.style.display = ""
-
+                                   item.style.display = ""                               
                                 }
-                }
-          
-
-
-              
-                               
-        
-
+                                
+                                          
+                } 
             
-            })                  
+        })  
 
+        filterIngredients ()                
+}
+
+
+
+
+function filterIngredients (ingredients) {
+
+   var inputIngredients =  document.querySelector('.input_ingredients');
+   var items = document.querySelector('.search-list').getElementsByTagName('li');
+   
+    inputIngredients.addEventListener('keyup', function(ev) {
+
+        var textIngredients = ev.target.value;
+        var patIngredients = new RegExp(textIngredients, 'i');
+    
+
+        for (var i=0; i < items.length; i++) {
+            var item = items[i];
+        
+            if (patIngredients.test(item.innerText)) {
+            item.classList.remove("hidden");
+            }
+            else {
+            item.classList.add("hidden");
+            }
+        }
+
+    });
+    
 }
